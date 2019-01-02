8ddd333bc3a76afb929aa576c533231add014f07
Umaieleo <umairjaved129@yahoo.com>
2019-01-02 23:29:00 +0500

    edit and updated feature added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,6 +19,19 @@ render 'new'
 end
 end
 
+def edit
+@recipe = Recipe.find(params[:id])
+end
+
+def update
+@recipe = Recipe.find(params[:id])
+if @recipe.update(recipe_params)
+    flash[:success]= "Recipe was updated successfully"
+redirect_to recipe_path(@recipe)
+else
+render 'edit'
+end
+end
 private
 def recipe_params
 params.require(:recipe).permit(:name, :description)
